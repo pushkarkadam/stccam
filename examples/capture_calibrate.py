3198d761c83cc52afa149bf2f992cca057b2d886
Pushkar Kadam <pushkarkadam17@outlook.com>
2025-06-13 15:22:55 +1000

    updated image_type parameter in example function

--- examples/capture_calibrate.py
+++ examples/capture_calibrate.py
@@ -7,4 +7,4 @@ from stccam import capture
 
 GENTL_PATH = '/opt/sentech/lib/libstgentl.cti'
 
-capture.capture_stereo_calibration(GENTL_PATH, save_path='../data/calib')
+capture.capture_stereo_calibration(GENTL_PATH,image_type=cv2.COLOR_BayerBG2BGR, save_path='../data/calib')
